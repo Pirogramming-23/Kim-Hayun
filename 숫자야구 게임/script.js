de3e2936606dc28d65d305cafb5b0773d7235146
hdavidkim4326 <hdavidkim@snu.ac.kr>
2025-07-02 18:18:35 +0900

    [feat] 스타라이크/볼 계산 로직

--- 숫자야구 게임/script.js
+++ 숫자야구 게임/script.js
@@ -1,1 +1,27 @@
+let answer;
 
+
+function initializeGameLogic() {
+    answer = [];
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    while (answer.length < 3) {
+        const randomIndex = Math.floor(Math.random() * numbers.length);
+        answer.push(numbers.splice(randomIndex, 1)[0]);
+    }
+    console.log('정답:', answer); 
+}
+
+function calculateResult(guess) {
+    let strikes = 0;
+    let balls = 0;
+    for (let i = 0; i < 3; i++) {
+        if (guess[i] === answer[i]) {
+            strikes++;
+        } else if (answer.includes(guess[i])) {
+            balls++;
+        }
+    }
+    return { strikes, balls };
+}
+
+initializeGameLogic();
