e8708ce99a18e8ea6b1c4ddcc03c2bf3480120e0
hdavidkim4326 <hdavidkim@snu.ac.kr>
2025-07-02 17:09:44 +0900

    JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,14 +1,31 @@
-function loadItems(){
-    return fetch('data/data.json')
-        .then(response=>response.json())
-        .then(json=>json.items);
 
+function loadItems() {
+  return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
+}
+
+// given items로 리스트 업데이트 
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  const html = items.map(item => createHTMLString(item));
+  container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+//given data item으로 html list item 만들기
+ function createHTMLString(item) {
+  return `
+    <li class="item">
+      <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
+      <span class="item_description">${item.gender}, ${item.size}</span>
+    </li>
+  `;
 }
 
 loadItems()
     .then(items=>{
         console.log(items);
-        // displayItems(items);
+        displayItems(items);
         // setEventListeners(items)
     })
     .catch(console.log);
