217a0e7504050c4fc60faa23318e2156329f9b28
hdavidkim4326 <hdavidkim@snu.ac.kr>
2025-07-12 00:45:10 +0900

    [feat] review_detail

--- myMovieReviews/movie_reviews_project/reviews/urls.py
+++ myMovieReviews/movie_reviews_project/reviews/urls.py
@@ -5,4 +5,5 @@ from reviews.views import *
 urlpatterns = [
     path('', review_list, name='review_list'),
     path('create/', review_create, name='review_create'),  
+    path('<int:pk>',review_detail, name='review_detail'),
 ]
--- myMovieReviews/movie_reviews_project/reviews/views.py
+++ myMovieReviews/movie_reviews_project/reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from .models import Movie
 
 # Create your views here.
@@ -9,10 +9,35 @@ def review_list(request):
   
   return render(request,"review_list.html",context)
 
+def review_detail(request, pk):
+    # 404 발생 추가함
+    movie = get_object_or_404(Movie, pk=pk)
+    context = {
+        "movie": movie,
+    }
+    return render(request, "review_detail.html", context)
 def review_create(request):
     if request.method == 'POST':
         title = request.POST.get('title')
+        year = request.POST.get('year')
+        genre = request.POST.get('genre')
+        stars = request.POST.get('stars')
+        runtime = request.POST.get('runtime')
         review = request.POST.get('review')
+        director = request.POST.get('director')
+        actors = request.POST.get('actors')
+        
+        Movie.objects.create(
+            title=title,
+            year=year,
+            genre=genre,
+            stars=stars,
+            runtime=runtime,
+            review=review,
+            director=director,
+            actors=actors,
+        )
+        
         return redirect('review_list')
 
     return render(request, "review_create.html")
