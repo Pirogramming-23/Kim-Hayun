cbb64beeedb7e459e7d64239a4d71b2d4d6c59bb
hdavidkim4326 <hdavidkim@snu.ac.kr>
2025-07-17 04:15:45 +0900

    [feat] ideas 기능 구현

--- SWIDEA_SITE/ideas/models.py
+++ SWIDEA_SITE/ideas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from devtools.models import DevTool
 
-# Create your models here.
+class Idea(models.Model):
+    title = models.CharField('제목', max_length=200)
+    #image = models.ImageField('이미지', blank=True, null=True, upload_to='ideas/%Y/%m/%d')
+    content = models.TextField('내용')
+    interest = models.IntegerField('관심도', default=0)
+    devtool = models.ForeignKey(DevTool, on_delete=models.SET_NULL, null=True, verbose_name='개발툴')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ideas') # 작성자
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class IdeaStar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+
+    class Meta:
+      unique_together = ('user', 'idea')
--- SWIDEA_SITE/ideas/views.py
+++ SWIDEA_SITE/ideas/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Idea, IdeaStar
+from .forms import IdeaForm
 
-# Create your views here.
+# 아이디어 목록
+def idea_list(request):
+    ideas = Idea.objects.all().order_by('-pk') # 최신순으로 정렬
+    context = {'ideas': ideas}
+    return render(request, 'ideas/idea_list.html', context)
+
+# 아이디어 등록
+@login_required
+def idea_create(request):
+    if request.method == 'POST':
+        form = IdeaForm(request.POST, request.FILES)
+        if form.is_valid():
+            idea = form.save(commit=False)
+            idea.author = request.user # 현재 로그인한 사용자를 작성자로 설정
+            idea.save()
+            return redirect('ideas:idea_detail', pk=idea.pk)
+    else:
+        form = IdeaForm()
+    context = {'form': form}
+    return render(request, 'ideas/idea_form.html', context)
+
+# 아이디어 읽기
+def idea_detail(request, pk):
+    idea = get_object_or_404(Idea, pk=pk)
+    is_starred = False
+    if request.user.is_authenticated:
+        if IdeaStar.objects.filter(user=request.user, idea=idea).exists():
+            is_starred = True
+            
+    context = {
+        'idea': idea,
+        'is_starred': is_starred,
+    }
+    return render(request, 'ideas/idea_detail.html', context)
+
+# 아이디어 수정
+@login_required
+def idea_update(request, pk):
+    idea = get_object_or_404(Idea, pk=pk)
+    # 작성자만 수정 가능
+    if request.user != idea.author:
+        return redirect('ideas:idea_list')
+
+    if request.method == 'POST':
+        form = IdeaForm(request.POST, request.FILES, instance=idea)
+        if form.is_valid():
+            form.save()
+            return redirect('ideas:idea_detail', pk=idea.pk)
+    else:
+        form = IdeaForm(instance=idea)
+    context = {'form': form, 'idea': idea}
+    return render(request, 'ideas/idea_form.html', context)
+
+# 아이디어 삭제
+@login_required
+def idea_delete(request, pk):
+    idea = get_object_or_404(Idea, pk=pk)
+  
+    if request.user == idea.author:
+        idea.delete()
+    return redirect('ideas:idea_list')
